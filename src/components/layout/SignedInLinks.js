628113be9f16cfc74ed1c20860a203635599bfa7
ramasatriafb <ramasatriafb@gmail.com>
2018-12-24 22:16:12 +0700

    Update Link Navbar Terhubung

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to ='/'>Buat Resep Baru</NavLink></li>
+            <li><NavLink to ='/buat-resep'>Buat Resep Baru</NavLink></li>
             <li><NavLink to ='/'>Log Out</NavLink></li>
             <li><NavLink to ='/' className='btn btn-floating green lighten-1'>NN</NavLink></li>
         </ul>
