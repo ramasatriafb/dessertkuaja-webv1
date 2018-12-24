dd9080fcc452ae2c4f1f6d557210a53df4ca4fdd
ramasatriafb <ramasatriafb@gmail.com>
2018-12-24 16:38:42 +0700

    Update Komponen Untuk Rangkuman Resep

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -1,49 +1,16 @@
  import React from 'react'
+ import RecipeSummary from './RecipeSummary'
 
  const RecipeList = () => {
      return (
          <div className="recipe-list section">
-            
-            <div className="card z-depth recipe-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Recipe Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text"> @$ Desember 2018</p>
-                </div>
-            </div>
-            
-            <div className="card z-depth recipe-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Recipe Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text"> @$ Desember 2018</p>
-                </div>
-            </div>
-            
-            <div className="card z-depth recipe-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Recipe Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text"> @$ Desember 2018</p>
-                </div>
-            </div>
 
-            <div className="card z-depth recipe-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Recipe Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text"> @$ Desember 2018</p>
-                </div>
-            </div>
-
-            <div className="card z-depth recipe-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Recipe Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text"> @$ Desember 2018</p>
-                </div>
-            </div>
-
+           <RecipeSummary /> 
+           <RecipeSummary /> 
+           <RecipeSummary /> 
+           <RecipeSummary /> 
+           <RecipeSummary /> 
+           
          </div>
      )
  }
---
+++ src/components/recipes/RecipeSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RecipeSummary = () => {
+    return (
+        <div className="card z-depth recipe-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Recipe Title</span>
+                <p> Posted by Me</p>
+                <p className="grey-text"> 24 Desember 2018</p>
+            </div>
+        </div>
+    )
+}
+
+export default RecipeSummary
