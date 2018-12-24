37848033bd68f67365a93ba994878dc73f8a2412
ramasatriafb <ramasatriafb@gmail.com>
2018-12-24 16:29:13 +0700

    Update Komponen Untuk Dashboard

---
+++ src/components/recipes/RecipeList.js
@@ -0,0 +1,51 @@
+ import React from 'react'
+
+ const RecipeList = () => {
+     return (
+         <div className="recipe-list section">
+            
+            <div className="card z-depth recipe-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Recipe Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text"> @$ Desember 2018</p>
+                </div>
+            </div>
+            
+            <div className="card z-depth recipe-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Recipe Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text"> @$ Desember 2018</p>
+                </div>
+            </div>
+            
+            <div className="card z-depth recipe-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Recipe Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text"> @$ Desember 2018</p>
+                </div>
+            </div>
+
+            <div className="card z-depth recipe-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Recipe Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text"> @$ Desember 2018</p>
+                </div>
+            </div>
+
+            <div className="card z-depth recipe-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Recipe Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text"> @$ Desember 2018</p>
+                </div>
+            </div>
+
+         </div>
+     )
+ }
+
+ export default RecipeList
