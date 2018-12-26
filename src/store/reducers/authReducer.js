f4e082d80628a7e0044fb65b18492cda04d4e13b
ramasatriafb <ramasatriafb@gmail.com>
2018-12-26 21:22:24 +0700

    Update Sign In Authentication Firebase

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,22 @@
 const initState = {}
 
 const authReducer = (state = initState, action) => {
-    return state
+    switch (action.type) {
+        case 'LOGIN_ERROR':
+            return {
+                ...state,
+                authError: 'Login Gagal'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('Login Sukses');
+            return {
+                ...state,
+                authError: null
+            }
+    
+        default:
+            return state
+    }
 }
 
 
